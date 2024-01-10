62eb853a967631e52ea4f984847206e734c9e0f7
DogancanKaratas <karatasdgncn21@gmail.com>
2024-01-10 22:12:21 +0300

    Add stockSlice to store and update Firms page

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,8 +1,25 @@
-
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { useEffect } from "react"
+import useStockCalls from "../service/useStockCalls"
 
 const Firms = () => {
+  const { getFirms, getSales } = useStockCalls()
+
+  useEffect(() => {
+    // getFirms()
+    // getSales()
+    getStocks("firms")
+    getStocks("sales")
+  }, [])
+
   return (
-    <div>Firms</div>
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firms
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+    </div>
   )
 }
 
