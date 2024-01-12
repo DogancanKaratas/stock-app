7174d95325822ac9a33fb74c7e3c650053a1c06a
DogancanKaratas <karatasdgncn21@gmail.com>
2024-01-12 20:24:55 +0300

    Refactor FirmModal and Firms components

--- src/components/FirmModal.jsx
+++ src/components/FirmModal.jsx
@@ -1,20 +1,13 @@
-import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { modalStyle } from "../styles/globalStyles";
 
-
-
-export default function FirmModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+export default function FirmModal({ open, handleClose }) {
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,39 +1,45 @@
-import Button from "@mui/material/Button"
-import Typography from "@mui/material/Typography"
-import { useEffect } from "react"
-import useStockCalls from "../service/useStockCalls"
-import { useSelector } from "react-redux"
-import { Grid } from "@mui/material"
-import FirmCard from "../components/FirmCard"
-import FirmModal from "../components/FirmModal"
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import useStockCalls from "../service/useStockCalls";
+import { useSelector } from "react-redux";
+import { Grid } from "@mui/material";
+import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/FirmModal";
 
 const Firms = () => {
   // const { getFirms, getSales } = useStockCalls()
-  const { getStocks } = useStockCalls()
-  const {firms} = useSelector((state) => state.stock)
+  const { getStocks } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     // getFirms()
-    getStocks("firms")
-  }, [])
-console.log(firms);
+    getStocks("firms");
+  }, []);
+  console.log(firms);
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
         Firms
       </Typography>
-      <Button variant="contained">New Firm</Button>
+      <Button variant="contained" onClick={handleOpen}>
+        New Firm
+      </Button>
 
-      <FirmModal />
+      <FirmModal open={open} handleClose={handleClose} />
       <Grid container gap={2} mt={3} justifyContent={"center"}>
-        {firms?.map((firm)=>(
-          <Grid item key={firm._id} >
+        {firms?.map((firm) => (
+          <Grid item key={firm._id}>
             <FirmCard firm={firm} />
           </Grid>
         ))}
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Firms
+export default Firms;
