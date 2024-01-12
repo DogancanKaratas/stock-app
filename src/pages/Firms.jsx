00f552bff394e6d60350e002e91f79eaed248274
DogancanKaratas <karatasdgncn21@gmail.com>
2024-01-12 20:14:46 +0300

    Add FirmModal component and styles

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -5,6 +5,7 @@ import useStockCalls from "../service/useStockCalls"
 import { useSelector } from "react-redux"
 import { Grid } from "@mui/material"
 import FirmCard from "../components/FirmCard"
+import FirmModal from "../components/FirmModal"
 
 const Firms = () => {
   // const { getFirms, getSales } = useStockCalls()
@@ -22,6 +23,8 @@ console.log(firms);
         Firms
       </Typography>
       <Button variant="contained">New Firm</Button>
+
+      <FirmModal />
       <Grid container gap={2} mt={3} justifyContent={"center"}>
         {firms?.map((firm)=>(
           <Grid item key={firm._id} >
