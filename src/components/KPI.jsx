cbf3d18b6e2fc36dbd06f06417eb7a0ebbcd073f
DogancanKaratas <karatasdgncn21@gmail.com>
2024-01-17 22:01:39 +0300

    Add KPI component and Charts component to Home page

--- src/components/KPI.jsx
+++ src/components/KPI.jsx
@@ -1,19 +1,27 @@
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn"
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
 import PaymentsIcon from "@mui/icons-material/Payments"
+import { deepPurple } from "@mui/material/colors"
+import { Avatar, Paper, Stack } from "@mui/material"
 
-const kpiData = [{
-    id:1,
-    title:"sales",
-    ammount:"16900",
-    icon:<MonetizationOnIcon/>,
-    bgColor:"red"
+const kpiData = [
+  {
+    id: 1,
+    title: "sales",
+    amount: "$16900",
+    icon: <MonetizationOnIcon />,
+    bgColor: deepPurple[100],
+    color: deepPurple[600],
+  },
+]
 
-
-}]
 const KPI = () => {
   return (
-    <div>KPI</div>
+    <Stack justifyContent="center" alignItems="center">
+      <Paper elevation={10}>
+        <Avatar sx={{ bgcolor: deepPurple[500] }}>N</Avatar>
+      </Paper>
+    </Stack>
   )
 }
 
